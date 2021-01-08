9db5dd4f6c5c9ae7f2e644a279de72450a95c658
justingodden <justingodden@gmail.com>
2021-01-08 12:09:50 +0000

    resonsiveness tweak

--- src/components/molecules/MiddleDashboard.js
+++ src/components/molecules/MiddleDashboard.js
@@ -1,23 +1,15 @@
 import React from 'react'
 import LineChart from '../atoms/LineChart'
 import BarChart from '../atoms/BarChart'
-import { chartExample1 } from "../atoms/TestOptions";
-import { Line } from 'react-chartjs-2';
 
 function MiddleDashboard() {
     return (
         <div className="middleDashboard">
             <div className="middleDashboard__itemLeft">
                 <LineChart />
-                {/* <Line
-                    data={chartExample1["data1"]}
-                    options={chartExample1.options}
-                  /> */}
-                {/* <div className="temp"></div> */}
             </div>
             <div className="middleDashboard__itemRight">
                 <BarChart />
-                {/* <div className="temp"></div> */}
             </div>
         </div>
     )
